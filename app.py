206978a648e24e641b9496dc43e069ab1b6a47dc
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-08-31 16:28:20 +0900

    add youtube dl

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
-
+import os
+from flask import Flask, abort, request, make_response, jsonify
+import functools
+import youtube_dl
 # print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
@@ -14,21 +17,40 @@ instructions = '''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
+# EB looks for an 'app' callable by default.
+app = Flask(__name__)
+
+def content_type(value):
+    def _content_type(func):
+        @functools.wraps(func)
+        def wrapper(*args,**kwargs):
+            if not request.headers.get("Content-Type") == value:
+                error_message = {
+                    'error': 'not supported Content-Type'
+                }
+                return make_response(jsonify(error_message), 400)
+
+            return func(*args,**kwargs)
+        return wrapper
+    return _content_type
 
 # add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
+app.add_url_rule('/', 'index', (lambda: header_text +
     say_hello() + instructions + footer_text))
 
 # add a rule when the page is accessed with a name appended to the site
 # URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
+app.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
 
+@app.route('/youtubeDlSubtitles', methods=['GET'])
+@content_type('application/json')
+def youtubeDlSubtitles():
+    
+    return make_response(request.get_data())
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
